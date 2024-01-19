4213799760a00c4e53d67413879426a5276a997a
Z-Eso0111 <116544726+Z-Eso0111@users.noreply.github.com>
2024-01-19 21:54:31 +0300

    Create text.txt.lua

---
+++ text.txt.lua
@@ -0,0 +1,20 @@
+    --------------------------------------------------------------------------
+
+    Kurulum [Cross.X] :
+    Python kuruyoruz.
+    [Not: Add python.exe to PATH seçeneğini açınız kurulum esnasında]
+    Ardından Install.bat i açıyoruz ve bekliyorz.
+    Kurulum bittikten sonra Program.bat i açıyoruz.
+    Açılan pencereden modumuzu seçiyoruz.
+    [Not: Kontroller konsolda yazmaktadır!]
+
+    --------------------------------------------------------------------------
+
+    Kurulum [MauseDelayReset] :
+    Python kuruyoruz.
+    [Not: Add python.exe to PATH seçeneğini açınız kurulum esnasında]
+    [Not2: Isterseniz Python.bat çalıştırabilirsiniz]
+    Ardından Req[Instal].bat i açıyoruz ve bekliyoruz.
+    Kurulum bittikten sonra Delay=0.py dosyasını çalıştırıyoruz.
+
+    --------------------------------------------------------------------------
